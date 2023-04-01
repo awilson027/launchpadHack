8a1b93f0d8d468c757c31ef7adf680d42ddb1ac3
Nate Skutnik <nateskutnik19@gmail.com>
2023-04-01 13:44:03 -0400

    FlatButton

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
+import FlatButton from "../util/Button";
 
 export function HomeScreen() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>Home!</Text>
+            //<FlatButton text='Pull Day: 1' onPress={props.handle} />
         </View>
     );
+
+
 }
