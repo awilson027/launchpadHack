903353c22dbd23978006b46c60e6d9a985292bc8
Nate Skutnik <nateskutnik19@gmail.com>
2023-04-01 13:31:31 -0400

    FlatButton

---
+++ util/Button.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity, Text, View } from 'react-native';
+
+export default function FlatButton({ text, onPress })
+{
+    return (
+        <TouchableOpacity onPress={onPress}>
+            <view style={styles.button}>
+                <Text style={styles.buttonText}>{ text }</Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        borderRadius: 8,
+        paddingVertical: 14,
+        paddingHorizontal: 10,
+        backgroundColor: '#CBFFC3',
+    },
+    buttonText: {
+        color: 'green',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontSize: 16,
+        textAllign: 'center'
+    }
+}}
