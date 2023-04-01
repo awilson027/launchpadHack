8a0091c7089f902310e21433f4541d1ba252942f
Nate Skutnik <nateskutnik19@gmail.com>
2023-04-01 15:25:18 -0400

    hi bro!

--- screens/Exercises.js
+++ screens/Exercises.js
@@ -4,24 +4,70 @@ import FlatButton from "../util/Button";
 
 // make groups of exercises; click on group to expand exercise; click on any exercise to open with option to add to workout
 
-export function ExercisesScreen() {
-    return (
-        <View style={styles.container}>
-            <SectionList
-                sections={[
-                    {title: "Back", data: ['Bent-Over Rows', 'Deadlift', 'Lat Pulldown']},
-                    {title: "Chest", data: ['Bench Press', 'Cable Crossovers', 'Incline Bench Press'],
-                    },
-                ]}
-                renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
-                renderSectionHeader={({section}) => (
-                    <Text style={styles.sectionHeader}>{section.title}</Text>
-                )}
-                keyExtractor={item => `basicListEntry-${item}`}
-            />
-        </View>
-    );
-}
+const workoutData = [  {    muscleGroup: "Chest",    workouts: [      {        name: "Bench Press",        description: "Lie flat on your back with a barbell, and lower it to your chest before pressing it back up.",        image: require("./images/bench-press.jpg")      },      {        name: "Push-Ups",        description: "Start in a plank position with your hands under your shoulders, and lower your body until your chest touches the ground.",        image: require("./images/push-ups.jpg")      },      {        name: "Chest Flys",        description: "Lie on your back with your arms outstretched, and bring your arms together above your chest.",        image: require("./images/chest-flys.jpg")      }    ]
+},
+    {
+        muscleGroup: "Back",
+        workouts: [
+            {
+                name: "Deadlifts",
+                description: "Start with a barbell on the ground, and lift it up to hip level with a straight back.",
+
+            },
+            {
+                name: "Pull-Ups",
+                description: "Hang from a bar with your hands shoulder-width apart, and pull yourself up until your chin is above the bar.",
+
+            },
+            {
+                name: "Rows",
+                description: "Hold a barbell with both hands, and pull it up to your chest while bending your elbows.",
+
+            }
+        ]
+    },
+    {
+        muscleGroup: "Legs",
+        workouts: [
+            {
+                name: "Squats",
+                description: "Stand with your feet shoulder-width apart, and lower your body as if you were sitting in a chair.",
+
+            },
+            {
+                name: "Lunges",
+                description: "Step forward with one leg and lower your body until your front thigh is parallel to the ground.",
+
+            },
+            {
+                name: "Deadlifts",
+                description: "Start with a barbell on the ground, and lift it up to hip level with a straight back.",
+
+            }
+        ]
+    },
+    {
+        muscleGroup: "Arms",
+        workouts: [
+            {
+                name: "Bicep Curls",
+                description: "Hold a dumbbell in each hand, and lift them up to your shoulders while keeping your elbows close to your body.",
+
+            },
+            {
+                name: "Tricep Dips",
+                description: "Sit on a bench with your hands behind you, and lower your body until your arms are at a 90-degree angle.",
+
+            },
+            {
+                name: "Hammer Curls",
+                description: "Hold a dumbbell in each hand with your palms facing each other, and lift them up to your shoulders.",
+
+            }
+        ]
+    }
+];
+
 
 const listOfExercises = [ { muscleGroup: "Chest" }]
 
